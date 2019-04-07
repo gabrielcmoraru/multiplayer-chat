86e2ceead0ed98a393422bbff91bcb0eda57a4e6
Gabriel C Moraru <gabrielcmoraru@gmail.com>
2019-04-07 14:19:27 +0100

    add assets; create default scene +  import animations with fbxLoader

--- public_html/threejs/game.js
+++ public_html/threejs/game.js
@@ -1,37 +1,124 @@
 class Game{
-  constructor(){
-      this.scene = new THREE.Scene();
-  this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+	constructor(){
+		if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
-  this.renderer = new THREE.WebGLRenderer();
-      this.renderer.setSize( window.innerWidth, window.innerHeight );
-      document.body.appendChild( this.renderer.domElement );
+		this.container;
+		this.player = { };
+		this.stats;
+		this.controls;
+		this.camera;
+		this.scene;
+		this.renderer;
 
-      const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-      const light = new THREE.DirectionalLight( 0xffffff );
-  light.position.set( 0, 20, 10 );
-      const ambient = new THREE.AmbientLight( 0x707070 ); // soft white light
+		this.container = document.createElement( 'div' );
+		this.container.style.height = '100%';
+		document.body.appendChild( this.container );
 
-  const material = new THREE.MeshPhongMaterial( { color: 0xffaa00 } );
+		const game = this;
 
-      this.cube = new THREE.Mesh( geometry, material );
+		this.assetsPath = '../assets/';
 
-      this.scene.add( this.cube );
-      this.scene.add( light );
-      this.scene.add( ambient );
+		this.clock = new THREE.Clock();
 
-  this.camera.position.z = 3;
+        this.init();
 
-      this.animate();
-  }
+		window.onError = function(error){
+			console.error(JSON.stringify(error));
+		}
+	}
 
-animate() {
-      const game = this;
-      requestAnimationFrame( function(){ game.animate(); } );
+	init() {
 
-      this.cube.rotation.x += 0.01;
-      this.cube.rotation.y += 0.01;
+		this.camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+		this.camera.position.set(112, 100, 400);
 
-      this.renderer.render( this.scene, this.camera );
-  }
+		this.scene = new THREE.Scene();
+		this.scene.background = new THREE.Color( 0xa0a0a0 );
+		this.scene.fog = new THREE.Fog( 0xa0a0a0, 200, 1000 );
+
+		let light = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+		light.position.set( 0, 200, 0 );
+		this.scene.add( light );
+
+		light = new THREE.DirectionalLight( 0xffffff );
+		light.position.set( 0, 200, 100 );
+		light.castShadow = true;
+		light.shadow.camera.top = 180;
+		light.shadow.camera.bottom = -100;
+		light.shadow.camera.left = -120;
+		light.shadow.camera.right = 120;
+		this.scene.add( light );
+
+		// ground
+		var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
+		mesh.rotation.x = - Math.PI / 2;
+		//mesh.position.y = -100;
+		mesh.receiveShadow = true;
+		this.scene.add( mesh );
+
+		var grid = new THREE.GridHelper( 2000, 40, 0x000000, 0x000000 );
+		//grid.position.y = -100;
+		grid.material.opacity = 0.2;
+		grid.material.transparent = true;
+		this.scene.add( grid );
+
+		// model
+		const loader = new THREE.FBXLoader();
+		const game = this;
+
+		loader.load( `${this.assetsPath}fbx/people/FireFighter.fbx`, function ( object ) {
+
+			object.mixer = new THREE.AnimationMixer( object );
+			game.player.mixer = object.mixer;
+			game.player.root = object.mixer.getRoot();
+
+			object.name = "FireFighter";
+
+			object.traverse( function ( child ) {
+				if ( child.isMesh ) {
+                    child.material.map = null;
+					child.castShadow = true;
+					child.receiveShadow = false;
+				}
+			} );
+
+			game.scene.add(object);
+			game.player.object = object;
+			game.player.mixer.clipAction(object.animations[0]).play();
+
+            game.animate();
+		} );
+
+		this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+		this.renderer.setPixelRatio( window.devicePixelRatio );
+		this.renderer.setSize( window.innerWidth, window.innerHeight );
+		this.renderer.shadowMap.enabled = true;
+		this.container.appendChild( this.renderer.domElement );
+
+        this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+        this.controls.target.set(0, 150, 0);
+        this.controls.update();
+
+		window.addEventListener( 'resize', function(){ game.onWindowResize(); }, false );
+	}
+
+	onWindowResize() {
+		this.camera.aspect = window.innerWidth / window.innerHeight;
+		this.camera.updateProjectionMatrix();
+
+		this.renderer.setSize( window.innerWidth, window.innerHeight );
+
+	}
+
+	animate() {
+		const game = this;
+		const dt = this.clock.getDelta();
+
+		requestAnimationFrame( function(){ game.animate(); } );
+
+		if (this.player.mixer!==undefined) this.player.mixer.update(dt);
+
+		this.renderer.render( this.scene, this.camera );
+
+	}
 }
